9edd7de61d1bdcafb4cac53a1aad25f5d178e8e7
coord_e <me@coord-e.com>
2022-06-26 11:54:51 +0900

    feat: send stdin when no files are provided

--- src/main.rs
+++ src/main.rs
@@ -19,10 +19,31 @@ async fn run(args: Args) -> anyhow::Result<()> {
         .await
         .context("unable to create client")?;
     let channel = serenity::model::id::ChannelId(args.channel_id);
-    channel
-        .send_files(&client.cache_and_http.http, &args.files, |m| m)
-        .await
-        .context("unable to send files")?;
+
+    if args.files.is_empty() {
+        use tokio::io::AsyncReadExt as _;
+
+        let mut reader = tokio::io::BufReader::new(tokio::io::stdin());
+        let mut buf = Vec::new();
+        reader
+            .read_to_end(&mut buf)
+            .await
+            .context("unable to read stdin")?;
+
+        channel
+            .send_files(
+                &client.cache_and_http.http,
+                [(buf.as_slice(), "stdin.txt")],
+                |m| m,
+            )
+            .await
+            .context("unable to send files")?;
+    } else {
+        channel
+            .send_files(&client.cache_and_http.http, &args.files, |m| m)
+            .await
+            .context("unable to send files")?;
+    }
 
     Ok(())
 }
